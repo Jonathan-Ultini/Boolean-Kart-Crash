4425c8da55cd02c684cdba83161fa7795b64dabb
Jonathan-Ultini <jonathanultini@gmail.com>
2024-07-17 03:59:47 +0200

    add coin bonus point

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const gridMatrix = [
 
 
 // impostazioni iniziali
-const kartPosition = { y: 7, x: 3};
+const kartPosition = { y: 7, x: 3 };
 let score = 0;
 let speed = 1000;
 
@@ -44,7 +44,7 @@ function renderGrid() {
       cell.className = 'cell';
 
       // se c'è qualcosa, aggiungi anche una classe con lo stesso nome
-      if(cellContent) cell.classList.add(cellContent)
+      if (cellContent) cell.classList.add(cellContent)
 
       // ora metti l'elemento nella griglia
       grid.appendChild(cell);
@@ -54,7 +54,7 @@ function renderGrid() {
 
 
 //funzione che raggruppa le operazioni di rendering
-function renderElements(){
+function renderElements() {
   //posiziono il kart
   placeKart();
 
@@ -66,29 +66,30 @@ function renderElements(){
 
 //#:FUNZIONI RELATIVE AL KART
 //funzione per posizionare il kart
-function placeKart(){
+function placeKart() {
   // Recuperiamo il valore della cella in cui dobbiamo posizionare il kart
   const contentBeforeKart = gridMatrix[kartPosition.y][kartPosition.x];
 
-  // Se c'è qualcosa allora è collisione
-  if(contentBeforeKart) gameOver();
+  // Se c'è monetina, prendi i punti bonus, altrimenti è collisione
+  if (contentBeforeKart === 'coin') getBonusPoints();
+  else if (contentBeforeKart) gameOver();
 
   // inserisco la classe cart, nella cella corrispondente alle coordinate di kartPosition
   gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
-} 
+}
 
 //Funzione per muovere il kart
-function moveKart(direction){
+function moveKart(direction) {
   //solleviamo il kart per spostarlo da un'altra parte
   gridMatrix[kartPosition.y][kartPosition.x] = '';
 
   //aggiorniamo le coordinate a seconda della direzione
-  switch(direction){
+  switch (direction) {
     case 'left':
-      if(kartPosition.x > 0) kartPosition.x--;
+      if (kartPosition.x > 0) kartPosition.x--;
       break;
     case 'right':
-      if(kartPosition.x < 6) kartPosition.x++;
+      if (kartPosition.x < 6) kartPosition.x++;
       break;
     default:
       gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
@@ -101,7 +102,7 @@ function moveKart(direction){
 
 //#:FUNZIONI RELATIVE AGLI OSTACOLI
 // Funzione per far scorrere gli ostacoli
-function scroolObstacles(){
+function scroolObstacles() {
   // Rimuoviamo temporaneamente il kart
   gridMatrix[kartPosition.y][kartPosition.x] = '';
 
@@ -120,7 +121,7 @@ function scroolObstacles(){
 
 
 //Funzione per mescolare gli elementi di una riga
-function shuffleElements(row){
+function shuffleElements(row) {
   // Algoritmo di Fisher-Yates
   for (let i = row.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -132,7 +133,7 @@ function shuffleElements(row){
 
 
 //#: FUNZIONE DI FINE PARTITA
-function gameOver(){
+function gameOver() {
   // interrompo il flusso di gioco
   clearInterval(gameLoop);
 
@@ -158,25 +159,45 @@ function incrementScore() {
 // Funzione che incrementa la velocità
 function incrementSpeed() {
   // Se non siamo già troppo veloci
-  if(speed > 100){
+  if (speed > 100) {
     // Interrompo il flusso attuale
     clearInterval(gameLoop);
-  
+
     // Decremento intervallo (aumentando così la velocità)
     speed -= 100;
-  
+
     // Rilanciamoo un nuovo flusso con la velocità aggiornata
     gameLoop = setInterval(runGameFlow, speed);
   }
 }
 
+
+//#: FUNZIONI RELATIVE AL BONUS
+// funzione per ottenere punti bonus
+function getBonusPoints() {
+  // incrementiamo il puntteggio di 30
+  score += 30;
+
+  // inseriamo il punteggio aggiornato in pagina
+  scoreCounter.innerText = score;
+
+  // Aggiungiamo la classe 'bonus' al contatore
+  scoreCounter.classList.add('bonus');
+
+  // Rimuoviamo la classe subito modo in modo da poterla riassegnare (e vedere l'effetto)
+  setTimeout(function () {
+    scoreCounter.classList.remove('bonus');
+  }, 1000)
+}
+
+
 //#: FUNZIONI RELATIVE AL FLUSSO DI GIOCO
 // Funzione che raggruppa le operazioni da ripetere ciclicamente
-function runGameFlow(){
+function runGameFlow() {
   // Aumentare il punteggio
   incrementScore();
   // Aumentare la velocità
-  if(score % 10 === 0) incrementSpeed();
+  if (score % 10 === 0) incrementSpeed();
   // Far scorrere gli ostacoli
   scroolObstacles();
 }
@@ -185,23 +206,23 @@ function runGameFlow(){
 
 // #: EVENTI DEL GIOCO
 //click sul bottone di sinistra
-playAgainButton.addEventListener('click' , function(){
+playAgainButton.addEventListener('click', function () {
   location.reload();
 });
 
 //click sul bottone di sinistra
-leftButton.addEventListener('click' , function(){
+leftButton.addEventListener('click', function () {
   moveKart('left');
 });
 
 //click sul bottone di destra
-rightButton.addEventListener('click' , function(){
+rightButton.addEventListener('click', function () {
   moveKart('right');
 });
 
 //reazione alle freccette
-document.addEventListener('keyup' , function(e){
-  switch(e.key) {
+document.addEventListener('keyup', function (e) {
+  switch (e.key) {
     case 'ArrowLeft':
       moveKart('left');
       break;
@@ -211,7 +232,7 @@ document.addEventListener('keyup' , function(e){
     default: return;
   }
 })
- 
+
 
 //#:ESECUZIONE DELLE FUNZIONI DI GIOCO
 // Scrollo automaticamente gli ostacoli
